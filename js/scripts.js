345d231beadef72e8629c344b413cf5eb598f021
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-08-22 15:01:16 -0300

    Add animation with scrollrevealjs

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,36 @@ toggleOpen.addEventListener('click', toggleMenu);
 toggleClose.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    menu.classList.toggle('show-menu');
-    if (menu.classList.contains('show-menu')) {
-        toggleOpen.style.display = 'none';
-        toggleClose.style.display = 'block';
-    } else {
-        toggleOpen.style.display = 'block';
-        toggleClose.style.display = 'none';
-    }
+  menu.classList.toggle('show-menu');
+  if (menu.classList.contains('show-menu')) {
+    toggleOpen.style.display = 'none';
+    toggleClose.style.display = 'block';
+  } else {
+    toggleOpen.style.display = 'block';
+    toggleClose.style.display = 'none';
+  }
 }
 
 window.onload = function () {
-    const $form = document.querySelector('#form');
-    $form.addEventListener('submit', handleSubmit);
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const form = new FormData(this);
-        const response = await fetch(this.action, {
-            method: this.method,
-            body: form,
-            headers: {
-                Accept: 'application/json',
-            },
-        })
-        if (response.ok) {
-            this.reset()
-            alert('Mensaje enviado');
-        }
+  const $form = document.querySelector('#form');
+  $form.addEventListener('submit', handleSubmit);
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const form = new FormData(this);
+    const response = await fetch(this.action, {
+      method: this.method,
+      body: form,
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    if (response.ok) {
+      this.reset();
+      alert('Mensaje enviado');
     }
+  }
+  ScrollReveal().reveal('#info-content', { delay: 500 });
+  ScrollReveal().reveal('#portfolio-content', { delay: 500 });
+  ScrollReveal().reveal('#skills-content', { delay: 500 });
+
 };

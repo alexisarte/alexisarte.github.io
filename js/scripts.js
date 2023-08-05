c72d02be410b37daa8f72557fe5b77b503fbd2ab
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-08-05 20:56:42 -0300

    Add active class to the current button (highlight it)

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ window.onload = function () {
   const toggleOpen = document.querySelector('#toggle-open');
   const toggleClose = document.querySelector('#toggle-close');
   const $form = document.querySelector('#form');
+  const links = document.querySelectorAll('.navbar a');
+
 
   /**
    * Function to show/hide the navigation bar on mobile devices
@@ -49,22 +51,23 @@ window.onload = function () {
   }
   $form.addEventListener('submit', handleSubmit);
 
-  /*global ScrollReveal*/
-  ScrollReveal().reveal('#info-content', { delay: 500 });
-  ScrollReveal().reveal('.cards-reveal', { interval: 500 });
-  ScrollReveal().reveal('.skills-reveal', { interval: 200 });
 
-  // let active = document.querySelectorAll('li');
-  // active.forEach((item) => {
-  //   item.addEventListener('click', (e) => {
-  //     active.forEach((item) => {
-  //       item.classList.toggle('active');
-  //     });
-  //   });
-  // });
+  // Add active class to the current button (highlight it)
 
+  links.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      console.log(item);
+      links.forEach((item) => {
+        item.classList.remove('active');
+      });
+      item.classList.add('active');
+    });
+  });
+
+
+  // Smooth scrolling
+  
   const navbarHeight = document.querySelector('.navbar').offsetHeight;
-  const links = document.querySelectorAll('.navbar a');
 
   links.forEach(link => {
     link.addEventListener('click', function(event) {
@@ -72,12 +75,18 @@ window.onload = function () {
       const target = document.querySelector(link.getAttribute('href'));
       const targetPosition = target.getBoundingClientRect().top;
       const offsetPosition = targetPosition - navbarHeight;
-      
+
       window.scrollBy({
         top: offsetPosition,
         behavior: 'smooth'
       });
     });
   });
+
+  
+  /*global ScrollReveal*/
+  ScrollReveal().reveal('#info-content', { delay: 500 });
+  ScrollReveal().reveal('.cards-reveal', { interval: 500 });
+  ScrollReveal().reveal('.skills-reveal', { interval: 200 });
 
 };
